6dfef350efc1b8cb7c1b1c96abd68e3ff191f3d2
Muhammed Salih Guler <gueler@karlmax-berlin.com>
2018-11-11 19:14:18 +0100

    Delete the base application and create Voluntiro widget.

---
+++ lib/voluntiroapp.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+///
+/// Main application widget for the Voluntiro application
+///
+class VoluntiroApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
